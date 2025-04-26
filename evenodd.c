bdd0763b555de7d03ab8fdc78e3974852a2ee294
Bijoy41 <bijoymoitro0@gmail.com>
2025-04-26 12:02:39 +0600

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age=25;
+    float hight =5.9;
+    char grade ='A';
+
+     printf("Age :%d \n Hight:%.1f \n Grade: %c\n",age,hight,grade);
+    return 0;
+}
