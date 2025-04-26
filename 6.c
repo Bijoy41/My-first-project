e46fa17e315c23333e8bfa255aecc653438e88e5
Bijoy41 <bijoymoitro0@gmail.com>
2025-04-26 12:01:15 +0600

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+int num = 10;
+
+float pi = 3.14;
+
+char letter = 'C';
+
+
+
+printf("Integer: %d, Float: %.2f, Character: %c\n", num, pi, letter);
+
+return 0;
+
+}
