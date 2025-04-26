059bcdb1ade92e92d47b7fb938c4a1973b0f0af3
Bijoy41 <bijoymoitro0@gmail.com>
2025-04-26 11:59:33 +0600

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float height = 5.9;
+    char grade = 'A';
+
+    printf("Age: %d, Height: %.1f, Grade: %c\n", age, height, grade);
+    return 0;
+}
+
+
